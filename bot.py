5f765d03be10c277491cac1aa388631a3d688482
Jake Sad <jakesad2212@gmail.com>
2021-10-20 17:30:22 -0500

    first.commit

---
+++ bot.py
@@ -0,0 +1,73 @@
+import discord
+import random
+from discord.ext import commands
+from discord.ext.commands.core import has_permissions
+
+client = commands.Bot(command_prefix = '&')
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+    responses = ["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."]
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+@has_permissions(kick_members = True)
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'{member} was kicked. Reason- {reason}')
+
+@client.command()
+@has_permissions(ban_members = True)
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'{member} was banned. Reason- {reason}')
+
+@client.command()
+@has_permissions(ban_members = True)
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
+                return
+
+            else:
+                await ctx.send('I could not find that user!')
+
+client.run('OTAwMDk3Njc0NjE3OTUwMzE4.YW8XSA.YuSQ91Xk4cQCYOB8JvZLGIFxxCQ')
